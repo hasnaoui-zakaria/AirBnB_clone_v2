5c9b87f93d2cbc8fce0f320e5967b3e5f3a77666
Habou Zakaria <habouzakaria@gmail.com>
2023-12-07 15:05:51 -0800

    remove files

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,14 +6,13 @@ import os
 
 
 def do_pack():
-        try:
-                    if not os.path.exists("versions"):
-                                    local('mkdir versions')
-                                            t = datetime.now()
-                                                    f = "%Y%m%d%H%M%S"
-                                                            archive_path = 'versions/web_static_{}.tgz'.format(t.strftime(f))
-                                                                    local('tar -cvzf {} web_static'.format(archive_path))
-                                                                            return archive_path
-        except:
-                    return None
-                
+    try:
+        if not os.path.exists("versions"):
+            local('mkdir versions')
+        t = datetime.now()
+        f = "%Y%m%d%H%M%S"
+        archive_path = 'versions/web_static_{}.tgz'.format(t.strftime(f))
+        local('tar -cvzf {} web_static'.format(archive_path))
+        return archive_path
+    except:
+        return None
